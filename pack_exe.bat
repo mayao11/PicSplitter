5d1879c01612ae006732a778c29d28cd9c9b73b7
mayao <279866129@qq.com>
2018-02-16 15:26:26 +0800

    发布易用的exe文件

--- pack_exe.bat
+++ pack_exe.bat
@@ -1,4 +1,11 @@
 pyinstaller TkPicSplitter.py --add-data "C:\Python36\tcl\tkdnd2.8";"tcl" -F
+if %errorlevel%==0 (
+del /f /q TkPicSplitter.spec
+rd /s /q build
+rd /s /q __pycache__
+)
+
 pause
+exit
 
 
