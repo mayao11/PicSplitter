cb433d335f45f14ccf7c7e0e563611ac7ab0c444
mayao <279866129@qq.com>
2018-02-16 13:37:31 +0800

    tk版本的切图调整工具基本功能OK

--- TkPicSplitter.py
+++ TkPicSplitter.py
@@ -1,22 +1,25 @@
 ﻿import re
 from PIL import Image, ImageTk, ImageChops
 import tkinter as tk
+from tkinter import messagebox
 from TkinterDnD2 import *
 
 root = TkinterDnD.Tk()
 
-#textRows = tk.StringVar()
-#textRows.set("1")
+gSelectedImageIndex = -1
+gBackImageIndex = tk.IntVar()
+gBackImageIndex.set(-1)
+gLblImage = None
+gBigPhotoImage = None
+gImageSizeVarX = tk.StringVar()
+gImageSizeVarY = tk.StringVar()
+gImageSizeEntryX = None
+gImageSizeEntryY = None
 
-selectedImageIndex = -1
-backImageIndex = tk.IntVar()
-backImageIndex.set(-1)
-lblImage = None
-bigPhotoImage = None
 
-def IsolateName(s):
-    l = s.split('.')
-    name = l[-2]
+def isolate_name(s):
+    t = s.split('.')
+    name = t[-2]
     it = re.finditer(r'\d+', name)
     match = list(it)[-1]
     num = match.group()
@@ -25,11 +28,12 @@ def IsolateName(s):
     ret = s[:match.start()] + num + s[match.end():]
     return ret
 
+
 class ImageFile(object):
-    def __init__(self, fileName):
-        self.image = Image.open(fileName)
-        self.fileName = fileName
-        self.isolateFileName = IsolateName(self.fileName)
+    def __init__(self, file_name):
+        self.image = Image.open(file_name)
+        self.fileName = file_name
+        self.isolateFileName = isolate_name(self.fileName)
         self.order = 0
 
         self.label = None
@@ -40,111 +44,202 @@ class ImageFile(object):
         self.offsetX = 0
         self.offsetY = 0
 
+
 class FusionImage(object):
     def __init__(self):
         self.image = None
         self.photoImage = None
         self.label = None
 
+
 images = []
 
-def ShowImages():
-    def _OnImageClick(i):
+
+def show_images():
+    def _on_image_click(index):
         def func(evt):
-            return OnImageClick(evt, i)
+            return on_image_click(evt, index)
         return func
+
+    def _on_radiobutton_click(index):
+        def func():
+            return on_radiobutton_click(index)
+        return func
+
     for i, imageFile in enumerate(images):
-        imgTemp = imageFile.image.copy()
-        imgTemp.thumbnail((100,100))
-        img = ImageTk.PhotoImage(imgTemp)
+        img_temp = imageFile.image.copy()
+        img_temp.thumbnail((100, 100))
+        img = ImageTk.PhotoImage(img_temp)
 
         frame = tk.Frame(root)
         lbl = tk.Label(frame, image=img)
         lbl.pack()
-        radio = tk.Radiobutton(frame, variable=backImageIndex, value=i)
+        radio = tk.Radiobutton(frame, variable=gBackImageIndex, value=i, command=_on_radiobutton_click(i))
         radio.pack()
 
-        lbl.bind("<Button-1>", _OnImageClick(i))
+        lbl.bind("<Button-1>", _on_image_click(i))
 
         imageFile.label = lbl
         imageFile.photoImage = img
         imageFile.frame = frame
         frame.pack(side=tk.LEFT)
-
-def OnImageClick(event, index):
-    global selectedImageIndex
-    print("OnImageClick", event, index)
-    selectedImageIndex = index
+    refresh_image_activate()
+    return
 
 
-def ClearImages():
+def refresh_image_activate():
+    global gSelectedImageIndex
+    for i, image_file in enumerate(images):
+        if i != gSelectedImageIndex:
+            image_file.label.config(bg="lightgrey")
+        else:
+            image_file.label.config(bg="blue")
+    return
+
+
+def on_image_click(event, index):
+    global gSelectedImageIndex
+    gSelectedImageIndex = index
+    refresh_image_activate()
+    move_image(0, 0)
+
+
+def on_radiobutton_click(index):
+    global gSelectedImageIndex
+    if gSelectedImageIndex == -1:
+        gSelectedImageIndex = index
+        refresh_image_activate()
+    move_image(0, 0)
+
+
+def clear_images():
     for i, imageFile in enumerate(images):
         imageFile.frame.destroy()
 
-def OnFileDrop(event):
+
+def on_file_drop(event):
     files = event.data.split()
-    ClearImages()
+    clear_images()
     for name in files:
         exist = False
-        for imageFile in images:
-            if imageFile.fileName == name:
+        for image_file in images:
+            if image_file.fileName == name:
                 exist = True
                 break
         if not exist:
-            imageFile = ImageFile(name)
-            images.append(imageFile)
-    images.sort(key=lambda im:im.isolateFileName)
-    for imageFile in images:
-        print(imageFile.fileName)
-    ShowImages()
+            image_file = ImageFile(name)
+            images.append(image_file)
+    images.sort(key=lambda im: im.isolateFileName)
+    for image_file in images:
+        print(image_file.fileName)
+        expand_global_size(image_file.image.size)
+    show_images()
 
 
-def MoveImage(ox, oy):
-    global bigPhotoImage
-    print("MoveImage", backImageIndex.get())
-    if selectedImageIndex == -1:
+def move_image(ox, oy):
+    global gBigPhotoImage
+    print("MoveImage", gBackImageIndex.get())
+    if gSelectedImageIndex == -1:
         return
 
-    if backImageIndex.get() != -1:
-        backFile = images[backImageIndex.get()]
-        if backFile.bigImage == None:
-            backFile.bigImage = Image.open(backFile.fileName)
-        back = backFile.bigImage
+    back = None
+    if gBackImageIndex.get() != -1 and gBackImageIndex.get() != gSelectedImageIndex:
+        back_file = images[gBackImageIndex.get()]
+        if back_file.bigImage is None:
+            back_file.bigImage = Image.open(back_file.fileName)
+            back_file.bigImage = expand_image(back_file.bigImage)
+        back = back_file.bigImage
+        back = ImageChops.offset(back, back_file.offsetX, back_file.offsetY)
 
-    imageFile = images[selectedImageIndex]
-    imageFile.offsetX += ox
-    imageFile.offsetY += oy
-    if imageFile.bigImage == None:
-        imageFile.bigImage = Image.open(imageFile.fileName)
-    temp = imageFile.bigImage
-    temp = ImageChops.offset(temp, imageFile.offsetX, imageFile.offsetY)
+    image_file = images[gSelectedImageIndex]
+    image_file.offsetX += ox
+    image_file.offsetY += oy
+    if image_file.bigImage is None:
+        image_file.bigImage = Image.open(image_file.fileName)
+    image_file.bigImage = expand_image(image_file.bigImage)
+    temp = image_file.bigImage
+    temp = ImageChops.offset(temp, image_file.offsetX, image_file.offsetY)
 
-    if back != None:
+    if back is not None:
         temp = ImageChops.blend(temp, back, 0.5)
 
     img = ImageTk.PhotoImage(temp)
-    bigPhotoImage = img
+    gBigPhotoImage = img
 
-    lblImage.config(image = img)
+    gLblImage.config(image=img)
 
     return
 
-def Start():
-    global lblImage
-    root.bind("<Up>", lambda evt:MoveImage(0, -1))
-    root.bind("<Down>", lambda evt:MoveImage(0, 1))
-    root.bind("<Left>", lambda evt:MoveImage(-1, 0))
-    root.bind("<Right>", lambda evt:MoveImage(1, 0))
-    lblDrag = tk.Label(root, text="将切好的图片拖到这里。（文件名不支持空格）")
-    lblDrag.drop_target_register(DND_FILES)
-    lblDrag.dnd_bind('<<Drop>>', OnFileDrop)
 
-    lblImage = tk.Label(root, text="...")
+def expand_image(image):
+    if not need_expand_image(image):
+        return image
+    new_image = Image.new(image.mode, global_size())
+    cur = image.size
+    new_image.paste(image, ((new_image.size[0]-cur[0])/2, (new_image.size[1]-cur[1])/2))
+    return new_image
 
-    lblDrag.pack(expand=1)
-    lblImage.pack()
+
+def global_size():
+    try:
+        x = int(gImageSizeVarX.get())
+        y = int(gImageSizeVarY.get())
+    except ValueError:
+        x, y = 0, 0
+    ret = [x, y]
+    return tuple(ret)
+
+
+def expand_global_size(t):
+    cur = global_size()
+    gImageSizeVarX.set(max(t[0], cur[0]))
+    gImageSizeVarY.set(max(t[1], cur[1]))
+
+
+def need_expand_image(image):
+    cur = global_size()
+    return image.size[0] < cur[0] or image.size[1] < cur[1]
+
+
+def on_save_all_click():
+    if not messagebox.askokcancel("全部保存", '保存文件会直接修改所有文件，是否继续？'):
+        return
+    for i, image_file in enumerate(images):
+        if image_file.bigImage is not None:
+            temp = image_file.bigImage
+            temp = ImageChops.offset(temp, image_file.offsetX, image_file.offsetY)
+            temp.save(image_file.fileName)
+            print("覆盖保存了", image_file.fileName)
+
+
+def start():
+    global gLblImage, gImageSizeEntryX, gImageSizeEntryY
+    root.bind("<Up>", lambda evt: move_image(0, -1))
+    root.bind("<Down>", lambda evt: move_image(0, 1))
+    root.bind("<Left>", lambda evt: move_image(-1, 0))
+    root.bind("<Right>", lambda evt: move_image(1, 0))
+    lbl_drag = tk.Label(root, text="将切好的图片拖到这里，名称要有序且不包含空格")
+    lbl_drag.drop_target_register(DND_FILES)
+    lbl_drag.dnd_bind('<<Drop>>', on_file_drop)
+
+    gLblImage = tk.Label(root, text="...")
+
+    lbl_drag.pack(expand=1)
+
+    def _test_digit(content):
+        return content == "" or content.isdigit()
+
+    test_cmd = root.register(_test_digit)
+    gImageSizeEntryX = tk.Entry(root, textvariable=gImageSizeVarX, validate='key', validatecommand=(test_cmd, '%P'))
+    gImageSizeEntryY = tk.Entry(root, textvariable=gImageSizeVarY, validate='key', validatecommand=(test_cmd, '%P'))
+    gImageSizeEntryX.pack()
+    gImageSizeEntryY.pack()
+
+    tk.Button(root, text='全部保存', command=on_save_all_click).pack()
+
+    gLblImage.pack()
     root.mainloop()
 
-if __name__ == '__main__':
-    Start()
 
+if __name__ == '__main__':
+    start()
--- pack_exe.bat
+++ pack_exe.bat
@@ -1,1 +1,4 @@
 pyinstaller TkPicSplitter.py --add-data "C:\Python36\tcl\tkdnd2.8";"tcl" -F
+pause
+
+
